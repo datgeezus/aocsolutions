01070129b7a069b0f37fab96dfc6297eb2b553f0
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-08-24 20:53:39 -0400

    parse boards

--- py/day4.py
+++ py/day4.py
@@ -74,3 +74,44 @@ to get the final score, 188 * 24 = 4512.
 To guarantee victory against the giant squid, figure out which board will win first.
 What will your final score be if you choose that board?
 """
+import logging as log
+
+def gen_numbers(input: list[str]) -> list[int]:
+    return list(map(int, input[0]))
+
+def get_board(input: list[str]):
+    n = len(input)
+    while n > 0:
+        yield [list(map(int, input[i].split())) for i in range(5)]
+        n -= 5
+
+def gen_boards(input: list[str]) -> dict[int,list]:
+    boards = {i:board for i,board in enumerate(get_board(input))}
+    log.warning(f"{boards}")
+    return boards
+
+
+if __name__ == "__main__":
+    test_input = [
+        "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+
+        "22 13 17 11  0",
+        " 8  2 23  4 24",
+        "21  9 14 16  7",
+        " 6 10  3 18  5",
+        " 1 12 20 15 19",
+
+        " 3 15  0  2 22",
+        " 9 18 13 17  5",
+        "19  8  7 25 23",
+        "20 11 10 24  4",
+        "14 21 16 12  6",
+
+        "14 21 17 24  4",
+        "10 16 15  9 19",
+        "18  8 23 26 20",
+        "22 11 13  6  5",
+        " 2  0 12  3  7",
+    ]
+
+    gen_boards(test_input[1::])
