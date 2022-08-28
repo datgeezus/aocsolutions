b869a3cf3f12ce39756a600d6ec1e168dea866c1
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-08-28 13:58:43 -0400

    added cmd parsing

---
+++ py/utils/args.py
@@ -0,0 +1,20 @@
+import argparse
+import logging
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Config:
+    log_level: str
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log-level",
+        default=logging.getLevelName(logging.WARNING),
+        choices=logging._nameToLevel.keys(),
+        help="Log level"
+    )
+
+    args = parser.parse_args()
+
+    return Config(args.log_level.upper())
