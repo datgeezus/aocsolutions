65c1279a3f2650da1ac721a7b7d67d47e7c6a1cb
Jesus Sanchez <jesus.san.sev@gmail.com>
2022-12-09 11:10:21 -0500

    day 9, part 2

--- 2022/py/day9.py
+++ 2022/py/day9.py
@@ -24,24 +24,31 @@ def get_head_coords(steps: list[tuple[str,int]]) -> list[Coord]:
 
     return visited
 
-def get_tail_coords(coords: list[Coord]) -> list[Coord]:
+def get_tail_coords(coords: list[Coord], min_dist: float, n_knots) -> list[Coord]:
     tail = []
     curr = coords[0]
 
     tail.append(curr)
     for i,coord in enumerate(coords[1:], start=1):
         dist = math.dist(curr, coord)
-        if dist > 1.5:
-            curr = coords[i-1]
-            tail.append(curr)
+        if dist > min_dist:
+            for k in range(n_knots, 0, -1):
+                curr = coords[i-k]
+                tail.append(curr)
 
     return tail
 
 def day9p1(steps: list[tuple[str,int]]) -> int:
+    min_dist = math.dist((0,0),(1,1))
     head = get_head_coords(steps)
-    tail = get_tail_coords(head)
+    tail = get_tail_coords(head, min_dist, 1)
     return len(set(tail))
 
+def day9p2(steps: list[tuple[str,int]]) -> int:
+    min_dist = math.dist((0,0),(9,9))
+    head = get_head_coords(steps)
+    tail = get_tail_coords(head, min_dist, 9)
+    return len(set(tail))
 
 
 def parse(inp: str) -> tuple[str,int]:
@@ -60,12 +67,34 @@ if __name__ == "__main__":
         "R 2"
     ]
 
+    print("--- Test 1 ---")
     input = list(map(parse, test_input))
     ans = day9p1(input)
+    print(ans)
+
+    print("--- Test 2 ---")
+    test_input_2 = [
+        "R 5",
+        "U 8",
+        "L 8",
+        "D 3",
+        "R 17",
+        "D 10",
+        "L 25",
+        "U 20"
+    ]
+    input = list(map(parse, test_input_2))
+    ans = day9p2(input)
     print(ans)
 
     print("--- Part 1 ---")
     input_raw = load_input.load("./inputs/input9.txt")
     input = list(map(parse, input_raw))
     ans = day9p1(input)
+    print(ans)
+
+    print("--- Part 2 ---")
+    input_raw = load_input.load("./inputs/input9.txt")
+    input = list(map(parse, input_raw))
+    ans = day9p2(input)
     print(ans)
